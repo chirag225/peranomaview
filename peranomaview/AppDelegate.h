181ea3a0a8e4219087741fbb2a1dfb157122a97b
Parmarth Mori <pm@Parmarths-Mac.local>
2014-04-16 00:41:08 +0530

    Initial Commit

---
+++ peranomaview/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  peranomaview
+//
+//  Created by Parmarth Mori on 4/16/14.
+//  Copyright (c) 2014 PMCommu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ peranomaview/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  peranomaview
+//
+//  Created by Parmarth Mori on 4/16/14.
+//  Copyright (c) 2014 PMCommu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
