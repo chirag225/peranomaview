0407f610effd8760182f9c0bb205927f38189167
chirag225 <chiragpateljgd@gmail.com>
2014-05-08 16:54:23 +0530

    gigui
    pjpupu

--- peranomaview/ViewController.m
+++ peranomaview/ViewController.m
@@ -34,4 +34,80 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)selectimage:(id)sender {
+    
+    
+    if (![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        actionsheet = [[UIActionSheet alloc]
+                       initWithTitle:@"Select a photo source" delegate:self
+                       cancelButtonTitle:@"Cancel"
+                       destructiveButtonTitle:nil
+                       otherButtonTitles:@"Choose An Existing Photo",nil];
+        actionsheet.actionSheetStyle = UIActionSheetStyleDefault;
+        [actionsheet showInView:self.view];
+        actionsheet.tag = 0;
+    }
+    
+    else {
+        actionsheet = [[UIActionSheet alloc]
+                       initWithTitle:@"Select a photo source" delegate:self
+                       cancelButtonTitle:@"Cancel"
+                       destructiveButtonTitle:nil
+                       otherButtonTitles:@"Choose An Existing Photo", @"Take A Photo", nil];
+        actionsheet.actionSheetStyle = UIActionSheetStyleDefault;
+        [actionsheet showInView:self.view];
+        actionsheet.tag = 1;
+        
+    }
+
+    
+}
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+    
+    switch (actionSheet.tag) {
+        case 0:
+            if (buttonIndex == 0) {
+                
+                NSLog(@"Album");
+                
+                _imagePicker.delegate = self;
+                _imagePicker.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+                _imagePicker.allowsEditing = YES;
+                [self presentViewController:_imagePicker animated:YES completion:NULL];
+            }
+            
+            
+            
+            break;
+        case 1:
+            if (buttonIndex == 0) {
+                
+                NSLog(@"Album");
+                _imagePicker.delegate = self;
+                _imagePicker.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+                _imagePicker.allowsEditing = YES;
+                [self presentViewController:_imagePicker animated:YES completion:NULL];
+                
+                
+            } else if (buttonIndex == 1) {
+                NSLog(@"Camera");
+                
+                _imagePicker.delegate = self;
+                _imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+                _imagePicker.allowsEditing = YES;
+                [self presentViewController:_imagePicker animated:YES completion:NULL];
+            }
+            break;
+    }
+}
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    UIImage *aChosenImage = info[UIImagePickerControllerOriginalImage];
+    
+    _imageview.image=aChosenImage;
+    
+    [_imagePicker dismissViewControllerAnimated:YES completion:nil];
+}
+
+
 @end
